195dfb200c2a76778de0cb775d1eb38fbb0f542a
Eoin <eoinlees@outlook.com>
2020-10-02 13:23:37 +0100

    for loops practice

---
+++ squares.py
@@ -0,0 +1,11 @@
+#For loops recap
+#Eoin Lees
+
+# for loops - alway have a list
+#l = [0,1,2,3,4,5,6,7,8,9]
+
+for i in range(20):
+    print(f"{i:2} {i*i:4}")
+       
+
+print("That's the whole list!")
